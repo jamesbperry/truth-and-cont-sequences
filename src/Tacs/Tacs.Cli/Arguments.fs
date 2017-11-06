ddd8b2c9a005a020854dd8ace3f8a75f59aa6e65
Brandon Perry <bperry@osisoft.com>
2017-11-05 22:38:42 -0600

    Expanded the CLI

--- src/Tacs/Tacs.Cli/Arguments.fs
+++ src/Tacs/Tacs.Cli/Arguments.fs
@@ -2,6 +2,13 @@
 
 open Argu
 
+type BoundaryStrategy =
+    | Interpolated = 0
+    | InsideInclusive = 1
+    | InsideExclusive = 2
+    | OutsideInclusive = 3
+    | OutsideExclusive = 4
+
 type CompressionStrategy =
     | None = 0
     | Custom = 1
@@ -36,26 +43,73 @@ with
         member this.Usage =
             match this with
             | ToDo -> "Not yet implemented."
+and CountSlidingWindowArgs =
+    | [<AltCommandLine("-c")>] Count of count:int
+with
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Count _ -> "The (maximum) number of events which will be included in each window."
+and WidthSlidingWindowArgs =
+    | [<AltCommandLine("-w")>] Width of width:string
+with
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Width _ -> "The size of each window."    
+and SlidingWindowArgs = 
+    | [<CliPrefix(CliPrefix.None)>] ByCount of ParseResults<CountSlidingWindowArgs>
+    | [<CliPrefix(CliPrefix.None)>] ByWidth of ParseResults<WidthSlidingWindowArgs>
+with
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | ByCount _ -> "Windows are sized by a maximum event count."
+            | ByWidth _ -> "Windows are sized by a fixed width."
+and HoppingWindowArgs = 
+    | [<AltCommandLine("-w")>] Width of width:string
+    | [<AltCommandLine("-cw")>] Hop of hop:string
+with
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Width _ -> "Width of each window."
+            | Hop _ -> "Windows are generated at this interval."
+and WindowedArgs =
+    | [<CliPrefix(CliPrefix.None)>] Sliding of ParseResults<SlidingWindowArgs>
+    | [<CliPrefix(CliPrefix.None)>] Hopping of ParseResults<HoppingWindowArgs>
+with
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Sliding _ -> "A window is generated for each event, with the event as the window end"
+            | Hopping _ -> "Windows advance by a constant position value"
 and AggregateArgs =
+    | [<CliPrefix(CliPrefix.None)>] Windowing of ParseResults<WindowedArgs> option
     | [<AltCommandLine("-o")>] Operation of operation:AggregationOperations
     | [<Last;AltCommandLine("-c")>] Config of config:string list
 with
     interface IArgParserTemplate with
         member this.Usage =
             match this with
+            | Windowing _ -> "Optional windowing strategy."
             | Operation _ -> "The operation to be applied."
             | Config _ -> "Parameters for the aggregation operation."
 and SliceArgs =
     | [<AltCommandLine("-s")>] Start of start:string option
+    | [<AltCommandLine("-ss")>] StartStrategy of startstrategy:BoundaryStrategy option
     | [<AltCommandLine("-c")>] Count of count:int option
     | [<AltCommandLine("-e")>] End of ``end``:string option
+    | [<AltCommandLine("-es")>] EndStrategy of endstrategy:BoundaryStrategy option
 with
     interface IArgParserTemplate with
         member this.Usage =
             match this with
             | Start _ -> "Truncate the beginning of the input stream."
+            | StartStrategy _ -> "How the start point is enforced."
             | Count _ -> "Return only this number of values. Positive number counts from start; negative counts from end."
             | End _ -> "Truncate the end of the input stream."
+            | EndStrategy _ -> "How the end point is enforced."
 and TacsArgs =
     | [<AltCommandLine("-i")>] Input of input:string option
     | [<AltCommandLine("-o")>] Output of output:string option
